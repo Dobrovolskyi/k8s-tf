ce645b840aa055d42583da4e4911827bad9dbcc2
dobrovolskyi <vdobrovolskyi@gmail.com>
2020-12-03 14:20:55 +0200

    master_auth removed

--- mian.tf
+++ mian.tf
@@ -7,10 +7,6 @@ resource "google_container_cluster" "hillel" {
   remove_default_node_pool = true
   initial_node_count       = var.initial_node_count
 
-  master_auth {
-    username = ""
-    password = ""
-
     client_certificate_config {
       issue_client_certificate = false
     }

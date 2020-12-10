99b39a2eabf4d5154faf3d3846a634dcb8983241
dobrovolskyi <vdobrovolskyi@gmail.com>
2020-12-10 19:14:17 +0200

    base64decode back in different format

--- providers.tf
+++ providers.tf
@@ -14,7 +14,7 @@ provider "kubernetes" {
   host             = "google_container_cluster.${var.name}.endpoint"
   token            = data.google_client_config.default.access_token
 
-  client_certificate     = "google_container_cluster.${var.name}.master_auth[0].client_certificate"
-  client_key             = "google_container_cluster.${var.name}.master_auth[0].client_key"
-  cluster_ca_certificate = "google_container_cluster.${var.name}.master_auth[0].cluster_ca_certificate"
+  client_certificate     = "${base64decode(google_container_cluster.var.name.master_auth[0].client_certificate)}"
+  client_key             = "${base64decode(google_container_cluster.var.name.master_auth[0].client_key)}"
+  cluster_ca_certificate = "${base64decode(google_container_cluster.var.name.master_auth[0].cluster_ca_certificate)}"
 }
